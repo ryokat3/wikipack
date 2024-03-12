2d177e644c4a3919bc9c3cc2e3822ba892b1920a
ryokat3 <ryokat3@gmail.com>
2024-03-12 05:36:12 +0000

    Fixed highlight

--- src/markdown/converter.ts
+++ src/markdown/converter.ts
@@ -1,6 +1,7 @@
 // import { marked, Slugger } from 'marked'
-import marked from 'marked'
-// import hljs from 'highlight.js'
+import marked, { Marked } from 'marked'
+import { markedHighlight } from "marked-highlight"
+import hljs from 'highlight.js'
 import { getFile } from "../data/FileTree"
 import { MarkdownFileType, FolderType } from "../data/FileTreeType"
 import { splitPath, getDir, addPath } from "../utils/appUtils"
@@ -75,7 +76,7 @@ function getRendererExtension(
         }
     }
 }
-/*
+
 const colorAndKeywordsRegex = /([#%](?:[0-9a-fA-F]{3,6}|\w+))\[(.*?)\]/
 
 function decodeUriOrEcho(uri: string) {
@@ -89,60 +90,49 @@ function decodeUriOrEcho(uri: string) {
         throw e
     }
 }
-*/
+
 
 export function getRenderer(rootFolder: FolderType,  filePath:string, isMarkdown:(fileName:string)=>boolean) {
-    return (text: string): string => {
-        // hljs.highlightAll()
+    const highlightExtension = markedHighlight({
+        langPrefix: 'hljs language-',
+        highlight(code, _lang, _info) {
 
-        // const myRenderer = new MyRenderer(rootFolder, filePath, isMarkdown)
-        
-        /*
-        marked.setOptions({
-            renderer: myRenderer            
-            // highlight: (code: string, _lang: string, callback?: (error: any, code: string) => void) => {
-            highlight: (code: string, _lang: string) => {
-                // NOTE: to avoid space character in lang
-                //    
-                const lang = decodeUriOrEcho(_lang)
+            // NOTE: to avoid space character in lang
+            //    
+            const lang = decodeUriOrEcho(_lang)
 
-                if ((!lang) || (lang.match(colorAndKeywordsRegex) === null)) {
-                    return hljs.highlightAuto(code, [lang]).value
+            if ((!lang) || (lang.match(colorAndKeywordsRegex) === null)) {
+                const language = hljs.getLanguage(lang) ? lang : 'plaintext'
+                return hljs.highlight(code, { language }).value
+            }
+
+            const escapedCode = code.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+
+            const highlightedCode = lang.split(';').reduce((codeByColor, colorAndKeywords) => {
+
+                const colorAndKeywordsMatch: RegExpMatchArray | null = colorAndKeywords.match(colorAndKeywordsRegex)
+                if (colorAndKeywordsMatch === null) {
+                    return codeByColor
                 }
+                else {
+                    const isTextHighlight: boolean = (colorAndKeywordsMatch[1][0] === '#')
+                    const color: string = colorAndKeywordsMatch[1].substring(1)
+                    const isRgb: boolean = (color.match(/^[0-9a-fA-F]{3,6}$/) !== null)
 
-                const escapedCode = code.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+                    return colorAndKeywordsMatch[2].split(',').reduce((codeByWord, keyword) => {
+                        return codeByWord.replace(new RegExp(keyword, 'gi'), `<span style="${(isTextHighlight) ? 'color' : 'background-color'}:${(isRgb) ? '#' : ''}${color}">${keyword}</span>`)
+                    }, codeByColor)
+                }
+            }, escapedCode)
 
-                const highlightedCode = lang.split(';').reduce((codeByColor, colorAndKeywords) => {
+            return `<pre><code>${highlightedCode}</code></pre>`
+        }
+    })
 
-                    const colorAndKeywordsMatch: RegExpMatchArray | null = colorAndKeywords.match(colorAndKeywordsRegex)
-                    if (colorAndKeywordsMatch === null) {
-                        return codeByColor
-                    }
-                    else {
-                        const isTextHighlight: boolean = (colorAndKeywordsMatch[1][0] === '#')
-                        const color: string = colorAndKeywordsMatch[1].substring(1)
-                        const isRgb: boolean = (color.match(/^[0-9a-fA-F]{3,6}$/) !== null)
-
-                        return colorAndKeywordsMatch[2].split(',').reduce((codeByWord, keyword) => {
-                            return codeByWord.replace(new RegExp(keyword, 'gi'), `<span style="${(isTextHighlight) ? 'color' : 'background-color'}:${(isRgb) ? '#' : ''}${color}">${keyword}</span>`)
-                        }, codeByColor)
-                    }
-
-                }, escapedCode)
-
-                return `<pre><code>${highlightedCode}</code></pre>`
-            },
-            pedantic: false,
-            gfm: true,
-            breaks: false,
-            sanitize: false,
-            smartLists: true,
-            smartypants: false,
-            xhtml: false
-        })
-        */
+    return (text: string): string => {
+        const highlightMarked = new Marked(highlightExtension)
         
-        marked.use({ renderer:getRendererExtension(rootFolder, filePath, isMarkdown) })
-        return marked.parse(text, { async: false} ) as string
+        highlightMarked.use({ renderer:getRendererExtension(rootFolder, filePath, isMarkdown) })
+        return highlightMarked.parse(text, { async: false} ) as string
     }
 }
